4e5e2631b958f490e9810d9d6cfb91de13c14e9c
weadid <isip_m.v.gorbutova@mpt.ru>
2022-12-10 16:44:06 +0300

    provod

---
+++ Проводник/ArrowMenu.cs
@@ -0,0 +1,48 @@
+﻿namespace Проводник
+{
+    class ArrowMenu
+    {
+         
+        public int position;
+        int min, max;
+
+        public ArrowMenu(int max)
+        {
+            min = 0;
+            this.max = max;
+        }
+
+        public void Down()
+        {
+            if (position + 1 < max)
+            {
+                EraseArrow();
+                DrawArrow(++position);
+            }
+        }
+
+        public void Up()
+        {
+            if (position > min)
+            {
+                EraseArrow();
+                DrawArrow(--position);
+            }
+        }
+
+        public void DrawArrow(int line)
+        {
+            position = line;
+            Console.SetCursorPosition(0, line);
+            Console.WriteLine("->");
+        }
+
+        private void EraseArrow()
+        {
+            Console.SetCursorPosition(0, position);
+            Console.Write("  ");
+        }
+
+        
+    }
+}
---
+++ Проводник/Program.cs
@@ -0,0 +1,135 @@
+﻿using System.Diagnostics;
+using Проводник;
+
+namespace проводник__0_00_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.CursorVisible = false;
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (var drive in drives)
+            {
+                Console.WriteLine("  " + drive + " " + drive.TotalSize + " байт свободно из " + drive.AvailableFreeSpace + " байт");
+            }
+            ArrowMenu arrowMenu = new ArrowMenu(drives.Length);
+            arrowMenu.DrawArrow(0);
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        arrowMenu.Down();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        arrowMenu.Up();
+                        break;
+                    case ConsoleKey.Enter:
+                        ShowDirectory(drives[arrowMenu.position].RootDirectory);
+                        break;
+
+                }
+
+
+            }
+
+
+        }
+
+        static void ShowDirectory(DirectoryInfo root)
+        {
+
+            FileSystemInfo[] infos = root.GetFileSystemInfos();
+            Console.SetCursorPosition(0, 0);
+            foreach (var info in infos)
+            {
+                Console.WriteLine("  " + info);
+            }
+            ArrowMenu Menu = new ArrowMenu(infos.Length);
+            Menu.DrawArrow(0);
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        Menu.Down();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Menu.Up();
+                        break;
+                    case ConsoleKey.Enter:
+                        FileSystemInfo info = infos[Menu.position];
+                        if (info is DirectoryInfo directory)
+                        {
+                            Console.Clear();
+                            GowDirectory(directory);
+                        }
+                        else
+                        {
+                            Process.Start(info.FullName);
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        string path = root.FullName;
+                        Console.SetCursorPosition(0, 0);
+                        Console.Clear();
+                        Console.WriteLine("  " + path);
+
+                        break;
+                }
+
+
+            }
+        }
+        static void GowDirectory(DirectoryInfo root)
+        {
+
+            FileSystemInfo[] inf = root.GetFileSystemInfos();
+            Console.SetCursorPosition(0, 0);
+            foreach (var info in inf)
+            {
+                Console.WriteLine("  " + info);
+            }
+            ArrowMenu aMenu = new ArrowMenu(inf.Length);
+            aMenu.DrawArrow(0);
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        aMenu.Down();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        aMenu.Up();
+                        break;
+                    case ConsoleKey.Enter:
+                        FileSystemInfo info = inf[aMenu.position];
+                        if (info is DirectoryInfo directory)
+                        {
+                            GowDirectory(directory);
+                        }
+                        else
+                        {
+                            Process.Start(info.FullName);
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        /*string path = root.FullName;
+                        Console.SetCursorPosition(0, 0);
+                        Console.Clear();
+                        Console.WriteLine("  " + path);*/
+                        Console.Clear();
+                        Main();
+                        break;
+                }
+
+
+            }
+        }
+
+
+
+
+    }
+}
